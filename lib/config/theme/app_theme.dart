397bd5e8f63020d637c412ddf7bd9da3858c145c
yerblues11 <kevinecameros02@gmail.com>
2024-10-05 09:28:36 -0600

    Second commit

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: const Color.fromARGB(255, 121, 28, 203), 
+  );
+}
